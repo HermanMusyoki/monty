b0252a32e40248f3fbb6d0d9aab7a88ca4c2e03a
HermanMusyoki <salmantso14@gmail.com>
2023-12-15 05:37:35 -0800

    free stack

--- freeStack.c
+++ freeStack.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
-* freeStack - a function that frees a doubly linked list
+* free_stack - a function that frees a doubly linked list
 * @head: pointer to the beginning of the stack
 */
 void free_stack(stack_t *head)
